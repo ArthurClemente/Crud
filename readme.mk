26322c0de66237a1b72a9745b5f89e695431f1ba
Arthur <arthurclementte@hotmail.com>
2019-12-07 11:33:26 -0300

    adicionado os arquivo de explicação do programa.

---
+++ readme.mk
@@ -0,0 +1,3 @@
+O programa tem o intuito de gerenciar o banco de dados de um ambiente escolar, 
+tendo as opções de adicionar e remover alunos e professores, alocar os professores paras as matérias disponíveis,
+gerenciamento do boletim dos alunos e a apresentação de relatórios dos professores.
